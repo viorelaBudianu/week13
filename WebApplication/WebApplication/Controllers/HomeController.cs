856b1488f5a40eee848761a68eca106771ba1ed4
ascutariu <andreiscutariu92@gmail.com>
2019-07-16 19:34:54 +0300

    add first website

---
+++ WebApplication/WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // simulate go to database
+            var model = new UserViewModel() { Email = "andrei@yahoo.com", UserName = "andrei"  };
+
+            return View(model);
+        }
+    }
+}
---
+++ WebApplication/WebApplication/Models/UserViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication.Models
+{
+    public class UserViewModel
+    {
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
