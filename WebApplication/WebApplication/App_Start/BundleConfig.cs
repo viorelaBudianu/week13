b1b412177470a240b6305545763723e02d3991b2
viorelaBudianu <viorela.hariga@yahoo.com>
2019-07-22 15:03:40 +0300

    Added Html helpers and JQuery for datetime picker

--- WebApplication/WebApplication/App_Start/BundleConfig.cs
+++ WebApplication/WebApplication/App_Start/BundleConfig.cs
@@ -14,6 +14,12 @@ namespace WebApplication
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+          "~/Scripts/jquery-ui-{version}.js"));
+            //css  
+            bundles.Add(new StyleBundle("~/Content/cssjqryUi").Include(
+                   "~/Content/jquery-ui.css"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
--- WebApplication/WebApplication/Models/StudentsViewModel.cs
+++ WebApplication/WebApplication/Models/StudentsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,18 +11,31 @@ namespace WebApplication.Models
         Masculin = 1,
         Feminin=2
     }
+
+    
     public class StudentsViewModel
     {
         public int ID { get; set; }
+        [Display(Name = "Nume")]
         public string Nume { get; set; }
+        [Display(Name = "Prenume")]
         public string Prenume { get; set; }
+        [Display(Name = "Data Nasterii")]
+        [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime DataNasterii { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
         public string Email { get; set; }
+        [Display(Name = "Telefon")]
         public string Telefon { get; set; }
         public Sex Sex { get; set; }
+        [Display(Name = "Adresa")]
         public string Adresa { get; set; }
+        [Display(Name = "Oras")]
         public string Oras { get; set; }
+        [Display(Name ="Cod Postal")]
         public uint CodPostal { get; set; }
+        [Display(Name ="Judet")]
         public string Judet { get; set; }
         public const string Tara = "Romania";
         public List<string> Hobbys = new List<string>();
